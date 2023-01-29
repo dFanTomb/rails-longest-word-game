b1115897ed523c72a993dd466ee3d133a7a39702
Darko Iliev <darkoilievsum@gmail.com>
2023-01-29 20:36:09 +0100

    not finished yet

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,22 @@
+class GamesController < ApplicationController
+  def new
+    @letters = ('a'..'z').to_a.shuffle[0..4].join
+  end
+
+  def score
+    # raise
+    @letters = params[:letters].split
+    @word = (params[:word] || '').upcase
+    @included = included?(@word, @letters)
+  end
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter)}
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
